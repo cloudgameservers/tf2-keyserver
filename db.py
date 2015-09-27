33da3ac6eaa05ee1dfb73fddb3f2a1922b76f658
Kyle Lexmond <github@kyl191.net>
2015-09-27 12:00:03 -0400

    Make the sqlite db a class

--- db.py
+++ db.py
@@ -1,46 +1,61 @@
 from __future__ import print_function
 import sqlite3
+import os
 
-def createDB(dbPath):
-  conn = sqlite3.connect(dbPath)
-  conn.text_factory = unicode
-  conn.row_factory = sqlite3.Row
-  c = conn.cursor()
-  c.execute("""CREATE TABLE tf2_keys (
-    "server_account" INTEGER PRIMARY KEY,
-    "server_token" TEXT,
-    "steam_account" TEXT,
-    "host" TEXT)""")
+class tf2db(Object):
 
-def startDB(dbPath):
-  try:
+  def __init__(self, dbPath="tf2_keys.db"):
+    # probably should do a check for dbpath being non-absolute...
+    self.dbPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), dbPath)
+    if not os.path.isfile(self.dbPath):
+      self.createDB(self.dbPath)
+    self.conn = sqlite3.connect(self.dbPath)
+    self.conn.text_factory = unicode
+    self.conn.row_factory = sqlite3.Row
+    self.c = self.conn.cursor()
+    self.checkDB()
+
+  @staticmethod
+  def createDB(dbPath):
     conn = sqlite3.connect(dbPath)
-    conn.text_factory = unicode
     c = conn.cursor()
-    c.execute("SELECT * from tf2_keys limit 1")
-  except sqlite3.OperationalError as e:
-    print("SQLite3 Error : Operational Error", e.args[0])
-  except sqlite3.DatabaseError as e:
-    print("SQLite3 Error : Database Error", e.args[0])
-  except sqlite3.Error as e:
-    print("SQLite 3: Unknown Error", e.args[0])
-  except:
-    print("Unknown Error")
-  return conn
+    c.execute("""CREATE TABLE tf2_keys (
+      "server_account" INTEGER PRIMARY KEY,
+      "server_token" TEXT,
+      "steam_account" TEXT,
+      "host" TEXT)""")
+    c.close()
+    conn.commit()
 
-def addKey(db, key):
-  try:
-    db.execute(u"INSERT into tf2_keys VALUES (?, ?, ?, ?)", key)
-  except sqlite3.Error as e:
-    print("SQLite 3: Unknown Error", e.args[0])
+  def checkDB(self):
+    try:
+      self.c.execute("SELECT * from tf2_keys limit 1")
+    except sqlite3.OperationalError as e:
+      print("SQLite3 Error : Operational Error", e.args[0])
+    except sqlite3.DatabaseError as e:
+      print("SQLite3 Error : Database Error", e.args[0])
+    except sqlite3.Error as e:
+      print("SQLite 3: Unknown Error", e.args[0])
+    except:
+      print("Unknown Error")
 
-def getKey(db, host):
-  try:
-    exists = db.execute(u"SELECT COUNT(*) from tf2_keys WHERE host = ?", [host]).fetchone()[0]
-    if exists != 1:
-      row = db.execute(u"SELECT server_account from tf2_keys WHERE host IS Null Limit 1").fetchone()
-      db.execute(u"UPDATE tf2_keys set host = :host where server_account = :account",
-        {"host": host, "account": row[0]})
-    return db.execute(u"SELECT * from tf2_keys WHERE host = ?", [host]).fetchone()
-  except sqlite3.Error as e:
-    print("SQLite 3: Unknown Error", e.args[0])
+  def addKey(self, key):
+    try:
+      self.c.execute(u"INSERT into tf2_keys VALUES (?, ?, ?, ?)", key)
+    except sqlite3.Error as e:
+      print("SQLite 3: Unknown Error", e.args[0])
+
+  def getKey(self, host):
+    try:
+      exists = self.c.execute(u"SELECT COUNT(*) from tf2_keys WHERE host = ?", [host]).fetchone()[0]
+      if exists != 1:
+        row = self.c.execute(u"SELECT server_account from tf2_keys WHERE host IS Null Limit 1").fetchone()
+        self.c.execute(u"UPDATE tf2_keys set host = :host where server_account = :account",
+          {"host": host, "account": row[0]})
+      return self.c.execute(u"SELECT * from tf2_keys WHERE host = ?", [host]).fetchone()
+    except sqlite3.Error as e:
+      print("SQLite 3: Unknown Error", e.args[0])
+
+  def close(self):
+    self.c.close()
+    self.conn.commit()
--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 #! /usr/bin/python
 from __future__ import print_function
-import db, os, sys, re
 import argparse
 import json
+import sys
 from collections import namedtuple
 
-tf2_key = namedtuple('tf2_key', "server_account server_token steam_account host")
+import tf2_db
 
-# Initial setup of DB
-# We keep the connection & cursor seperate so we can do commits when we want
-dbPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tf2_keys.db")
-if not os.path.isfile(dbPath):
-  db.createDB(dbPath)
-dbconn = db.startDB(dbPath)
-dbcursor = dbconn.cursor()
-# End initial setup
+tf2_key = namedtuple('tf2_key', "server_account server_token steam_account host")
+db = tf2db()
 
 parser = argparse.ArgumentParser(description='Manage TF2 keys')
 parser.add_argument('-a', '--add', nargs=3,
@@ -25,13 +19,12 @@ args = parser.parse_args()
 
 if args.add:
   key = tf2_key(int(args.add[0]), args.add[1], args.add[2], args.host)
-  db.addKey(dbcursor, key)
+  db.addKey(key)
 elif args.host:
-  key = db.getKey(dbcursor, args.host)
+  key = db.getKey(args.host)
   for entry in key:
     print(entry)
 else:
   sys.exit(1)
-# Close the cursor & commit the DB one last time just for good measure
-dbcursor.close()
-dbconn.commit()
+
+db.close()
