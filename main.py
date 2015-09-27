3be324deb0cd67bc0ea88a3e55456ef70dc24f4e
Kyle Lexmond <github@kyl191.net>
2015-09-27 02:32:15 -0400

    tab to space & fix path to DB issue

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from __future__ import print_function
 import db, os, sys, re
 import argparse
 import json
-from os.path import join
 from collections import namedtuple
 
 tf2_key = namedtuple('tf2_key', "server_account server_token steam_account host")
 
 # Initial setup of DB
 # We keep the connection & cursor seperate so we can do commits when we want
-dbPath = os.path.abspath("tf2_keys.db")
+dbPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tf2_keys.db")
 if not os.path.isfile(dbPath):
-	db.createDB(dbPath)
+  db.createDB(dbPath)
 dbconn = db.startDB(dbPath)
 dbcursor = dbconn.cursor()
 # End initial setup
@@ -25,11 +24,11 @@ parser.add_argument('host', nargs='?', help="Host to retrieve keys for")
 args = parser.parse_args()
 
 if args.add:
-	key = tf2_key(int(args.add[0]), args.add[1], args.add[2], args.host)
-	db.addKey(dbcursor, key)
+  key = tf2_key(int(args.add[0]), args.add[1], args.add[2], args.host)
+  db.addKey(dbcursor, key)
 elif args.host:
-	key = db.getKey(dbcursor, args.host)
 	print(json.dumps(tf2_key(*key)))
+  key = db.getKey(dbcursor, args.host)
 else:
   sys.exit(1)
 # Close the cursor & commit the DB one last time just for good measure
